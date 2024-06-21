cdfea606f99fc0aabe5adc5c04455b9ff7c4ad29
Rammohan Sharma <rammohansharma.tech@gmail.com>
2024-06-22 01:22:22 +0530

    pass stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
   public static void main(String[] args) {
@@ -11,7 +13,10 @@ public class Main {
        serverSocket = new ServerSocket(4221);
        serverSocket.setReuseAddress(true);
        clientSocket = serverSocket.accept(); // Wait for connection from client.
+       OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream(), StandardCharsets.UTF_8);
        System.out.println("accepted new connection");
+       writer.write("HTTP/1.1 200 OK\r\n\r\n");
+       writer.flush();
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
