adafb9d98d9d993dc97c8ad98790f9e6d0a58a72
Rammohan Sharma <rammohansharma.tech@gmail.com>
2024-06-27 02:14:32 +0530

    added path handling

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -15,8 +17,16 @@ public class Main {
        clientSocket = serverSocket.accept(); // Wait for connection from client.
        OutputStreamWriter writer = new OutputStreamWriter(clientSocket.getOutputStream(), StandardCharsets.UTF_8);
        System.out.println("accepted new connection");
+       BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+       String line = reader.readLine();
+       String path = line.split(" ")[1];
+       if(path != null && path.equalsIgnoreCase("/"))
        writer.write("HTTP/1.1 200 OK\r\n\r\n");
+       else
+           writer.write("HTTP/1.1 404 Not Found\r\n\r\n");
        writer.flush();
+       clientSocket.close();
      } catch (IOException e) {
        System.out.println("IOException: " + e.getMessage());
      }
