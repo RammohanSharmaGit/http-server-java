da3fad42e466e62a726636c40608c5cf790eb8b1
Rammohan Sharma <rammohansharma.tech@gmail.com>
2024-07-02 00:56:42 +0530

    return use-agent

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,8 @@ import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
@@ -19,13 +21,21 @@ public class Main {
        System.out.println("accepted new connection");
        BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-       String line = reader.readLine();
+       List<String> request = new ArrayList<>();
+       String buffer;
+       while((buffer = reader.readLine())!=null && !buffer.isEmpty())
+        request.add(new String(buffer));
+       String line = request.get(0);
        String path = line.split(" ")[1];
        String [] paths = path.split("/");
        if(path != null && path.equalsIgnoreCase("/"))
        writer.write("HTTP/1.1 200 OK\r\n\r\n");
        else if(paths.length > 2 && path.split("/")[1].equalsIgnoreCase("echo")) {
            String res = path.split("/")[2];
+           writer.write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+ res.length()+"\r\n\r\n" + res);
+       }
+       else if(paths.length > 1 && path.split("/")[1].equalsIgnoreCase("user-agent")) {
+           String res = request.get(1).split("/r/n")[0].split(" ")[1];
            writer.write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+ res.length()+"\r\n\r\n" + res);
        }
        else
