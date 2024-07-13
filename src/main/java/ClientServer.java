684c46b69a97d73099bfeb27baf6588c44bf16f1
Rammohan Sharma <rammohansharma.tech@gmail.com>
2024-07-13 22:52:06 +0530

    file reading git issue

--- src/main/java/ClientServer.java
+++ src/main/java/ClientServer.java
@@ -31,29 +31,35 @@ public class ClientServer implements Runnable{
                 request.add(new String(buffer));
             System.out.println(request);
             String line = request.get(0);
-            String path = line.split(" ")[1];
-            String[] paths = path.split("/");
+            String httpMethod = line.split(" ")[0];
+            String resourcePath = line.split(" ")[1];
+            String[] paths = resourcePath.split("/");
             System.out.println(Arrays.toString(paths));
-            if (path != null && path.equalsIgnoreCase("/"))
+            if (resourcePath != null && resourcePath.equalsIgnoreCase("/"))
                 writer.write("HTTP/1.1 200 OK\r\n\r\n");
-            else if (paths.length > 2 && path.split("/")[1].equalsIgnoreCase("echo")) {
-                String res = path.split("/")[2];
+            else if (paths.length > 2 && resourcePath.split("/")[1].equalsIgnoreCase("echo")) {
+                String res = resourcePath.split("/")[2];
                 writer.write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + res.length() + "\r\n\r\n" + res);
-            } else if (paths.length > 1 && path.split("/")[1].equalsIgnoreCase("user-agent")) {
+            } else if (paths.length > 1 && resourcePath.split("/")[1].equalsIgnoreCase("user-agent")) {
                 String res = request.get(2).split("/r/n")[0].split(" ")[1];
                 writer.write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + res.length() + "\r\n\r\n" + res);
-            } else if (paths.length > 2 && path.split("/")[1].equalsIgnoreCase("files")) {
-                String filename = path.split("/")[2];
-
+            } else if (paths.length > 2 && resourcePath.split("/")[1].equalsIgnoreCase("files")) {
+                String filename = resourcePath.split("/")[2];
                 Path filePath = Paths.get(directory,filename);
-                System.out.println(filePath);
-                System.out.println(Files.exists(filePath));
                 if(Files.exists(filePath)){
                 String fileText = Files.readString(filePath);
                 writer.write("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: " + fileText.length() + "\r\n\r\n" + fileText);
                 } else {
                 writer.write("HTTP/1.1 404 Not Found\r\n\r\n");
                 }
+            } else if (httpMethod.equalsIgnoreCase("post") && paths.length > 2 && resourcePath.split("/")[1].equalsIgnoreCase("files")) {
+                String filename = resourcePath.split("/")[2];
+                Path filePath = Paths.get(directory,filename);
+                String fileText = request.get(6);
+                Files.createFile(filePath);
+                Files.writeString(filePath,fileText);
+                writer.write("HTTP/1.1 201 Created\r\n\r\n");
+
             } else {
                 writer.write("HTTP/1.1 404 Not Found\r\n\r\n");
             }
