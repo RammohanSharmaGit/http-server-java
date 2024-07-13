a3bff9133c1e4bf3600cbd534d972a09d7ede153
Rammohan Sharma <rammohansharma.tech@gmail.com>
2024-07-13 13:09:51 +0530

    file reading git issue

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,9 @@ public class Main {
   public static void main(String[] args) {
      ServerSocket serverSocket = null;
      Socket clientSocket = null;
+     String directory = "";
+     if(args.length>1)
+         directory = args[1];
 
      try {
        serverSocket = new ServerSocket(4221);
@@ -14,7 +17,7 @@ public class Main {
        while(true) {
 
            clientSocket = serverSocket.accept(); // Wait for connection from client.
-           new Thread(new ClientServer(clientSocket)).run();
+           new Thread(new ClientServer(clientSocket,directory)).run();
        }
 
      } catch (IOException e) {
