6c0af3e84ec07768ddb8a777a9ee65d9839faf99
Rammohan Sharma <rammohansharma.tech@gmail.com>
2024-07-15 21:06:47 +0530

    gzip compression

--- src/main/java/ClientServer.java
+++ src/main/java/ClientServer.java
@@ -59,9 +59,14 @@ public class ClientServer implements Runnable{
                     gzipOutputStream.close();
                     byte [] compressed = os.toByteArray();
                     os.close();
-                    System.out.println(Base64.getEncoder().encodeToString(compressed));
+                    StringBuilder sb = new StringBuilder();
+                    for (byte b : compressed) {
+                        sb.append(String.format("%02X",b));
+                    }
+                    String compressedString = sb.toString();
+                    System.out.println(compressedString);
                     writer.write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip \r\nContent-Length: "+ compressed.length  + "\r\n\r\n"
-                    + Base64.getEncoder().encodeToString(compressed));
+                    + compressedString);
               //  }
               //  else if (encodings.contains("gzip")){
               //      writer.write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Encoding: gzip \r\n\r\n");
